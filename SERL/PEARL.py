a0a96ff60fcb935846a6d4fd4d64b890661a2537
Joel Joseph <joeljosephjin@gmail.com>
2020-07-25 14:55:41 +0530

    second

---
+++ SERL/PEARL.py
@@ -0,0 +1,6 @@
+class PEARL:
+	def __init__(self):
+		self.nothing = 0
+
+	def printer(self):
+		print("it works! ", self.nothing)
---
+++ test.py
@@ -0,0 +1,4 @@
+from SERL.PEARL import PEARL
+
+pearl = PEARL()
+pearl.printer()
